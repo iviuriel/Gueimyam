0cfcf0811331f1d5de9e356928fccf17453962cb
Ivan Uriel Jimenez <iviuriel.pictures@gmail.com>
2020-09-26 00:52:25 +0200

    Apartado grafico minijuego conchas

--- Assets/Scripts/Minigame Shells/Shell.cs
+++ Assets/Scripts/Minigame Shells/Shell.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public int id; 
+    public bool selected = false;
+
+    public void DestroySelf(){
+        Destroy(this.gameObject);
+    }
+
+    public void SetSelectedTrue(){selected = true;}
+    public void SetSelectedFalse(){selected = false;}
 }
