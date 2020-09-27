208c99e9a390ecfab8949cb67f5a17cbc7e68a91
Laura Rodríguez <31097119+laurarodriguezcorpas@users.noreply.github.com>
2020-09-27 18:52:39 +0200

    Stars Minigame

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //--------------ROTATION-----------------
+    //---------------------------------------
+    private Vector3 localRotation;
+
+    [Header("Rotation values")]
+    [Tooltip("Increases the sensibility of the mouse in X times.")]
+    [Range(0, 10)]
+    public float mouseSensitivity = 4f;
+    [Tooltip("Increases the sensibility of the scrollwheel in X times.")]
+    [Range(0, 10)]
+    public float scrollSensitivity = 2f;
+    [Tooltip("Rotation speed of the camera")]
+    [Range(0, 20)]
+    public float orbitSpeed = 10f;
+    [Tooltip("Movement speed of the camera to zoom")]
+    [Range(0, 10)]
+    public float scrollSpeed = 6f;
+
+    [Header("Start Rotation")]
+    [Tooltip("Start rotation of the pivot on X-axis")]
+    public float startRotationVertical;
+    [Tooltip("Start rotation of the player on Y-axis")]
+    public float startRotationHorizontal;
+
+    [Header("Max Rotation values")]
+    [Tooltip("Minimum rotation of the pivot on X-axis in degrees.")]
+    [Range(0, 180)]
+    public float minVerticalRotation = 10f;
+
+     [Tooltip("Maximum rotation of the pivot on X-axis in degrees.")]
+    [Range(0, 180)]
+    public float maxVerticalRotation = 50f;
+    public bool ableToRotate { get; set; }
+
+    private Transform cameraPivot;
+
+    //--------------MOVEMENT-----------------
+    //---------------------------------------
+    //Movement values
+    private float movVertValue;
+    private float movHorValue;
+
+    [Header("Movement")]
+    [Tooltip("Speed of the main character")]
+    public float speed = 15f;
+
+    //---------------GLOBAL------------------
+    //---------------------------------------
+    private Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        cameraPivot = transform.GetChild(0);
+    }
+    void Start()
+    {
+        localRotation.y = startRotationVertical;
+        localRotation.x = startRotationHorizontal;
+        ableToRotate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movVertValue = Input.GetAxis("Vertical");
+        movHorValue = Input.GetAxis("Horizontal");
+    }
+
+    void LateUpdate(){
+        Rotate();
+    }
+
+    void FixedUpdate(){
+        Move();
+    }
+
+    void Rotate(){
+        
+        if (Input.GetMouseButton(1) && ableToRotate)
+        {
+            localRotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+            localRotation.y += Input.GetAxis("Mouse Y") * mouseSensitivity * -1f ;
+
+            localRotation.y = Mathf.Clamp(localRotation.y, minVerticalRotation, maxVerticalRotation);
+        }
+
+        Quaternion QT = Quaternion.Euler(0, localRotation.x, 0);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, QT, Time.deltaTime * orbitSpeed);
+
+        Quaternion QTPivot = Quaternion.Euler(localRotation.y, 0, 0);
+        cameraPivot.localRotation = Quaternion.Lerp(cameraPivot.localRotation, QTPivot, Time.deltaTime * orbitSpeed);
+        cameraPivot.localPosition = new Vector3(0f, 0f, cameraPivot.localEulerAngles.x/-2);
+    }
+
+    void Move(){
+        Vector3 moveOnLocal = transform.TransformDirection(new Vector3(movHorValue, 0f, movVertValue));
+        Vector3 movement = moveOnLocal * speed * Time.deltaTime;
+        rigidBody.MovePosition(rigidBody.position + movement);
+    }
+}
---
+++ Scripts/TestIce.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestIce : MonoBehaviour
+{
+    private bool slicing;
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slicing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!slicing){
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.forward * 10;
+                slicing = true;
+                direction = Vector3.forward;
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.left * 10;
+                slicing = true;
+                direction = Vector3.left;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.back * 10;
+                slicing = true;
+                direction = Vector3.back;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.right * 10;
+                slicing = true;
+                direction = Vector3.right;
+            } 
+        }else{
+            Vector3 rayorigin = transform.position - new Vector3(0f, 0.5f, 0f);
+            //Raycast al suelo para comprobar que está pisando
+            RaycastHit hitDown;
+            Debug.DrawRay(transform.position, Vector3.down, Color.green);
+            if (Physics.Raycast(transform.position, Vector3.down, out hitDown, 3))
+            {            
+                if (hitDown.collider != null)
+                {
+                    if (hitDown.collider.CompareTag("Snow"))
+                    {
+                        transform.position = new Vector3(hitDown.collider.transform.position.x, transform.position.y, hitDown.collider.transform.position.z);
+                        GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        slicing = false;  
+                        direction = Vector3.zero;             
+                    }                 
+                }
+            }
+
+            //Raycast en la dirección por si se mueve
+            RaycastHit hit;
+            Debug.DrawRay(transform.position, direction, Color.red);
+            if (Physics.Raycast(rayorigin ,direction, out hit, 3))
+            {            
+                if (hit.collider != null)
+                {
+                    if (hit.collider.CompareTag("Wall") || hit.collider.CompareTag("Rock"))
+                    {
+                        Debug.Log("hajsdfhjaskldfhjkasdf");
+                        transform.position = new Vector3(hitDown.collider.transform.position.x, transform.position.y, hitDown.collider.transform.position.z);
+                        GetComponent<Rigidbody>().velocity = Vector3.zero;
+                        slicing = false;     
+                        direction = Vector3.zero;            
+                    }                 
+                }
+            }
+        }  
+    }
+
+    /*void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Wall")){
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            slicing = false;
+        }
+    }*/
+}
